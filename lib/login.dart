7c5e8e5c22ed66b3f26161f45c0b72ed89084ff1
siddhamjain02 <jainsiddham420@gmail.com>
2022-01-24 14:24:57 +0530

    vsc

--- lib/login.dart
+++ lib/login.dart
@@ -35,7 +35,8 @@ class _LoginState extends State<Login> {
           password: passwordController.text,
         );
         if (res.isSignedIn = true) {
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => Home()));
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => Home()));
           setState(() {
             loading = false;
           });
@@ -44,9 +45,6 @@ class _LoginState extends State<Login> {
         setState(() {
           loading = false;
         });
-        print("****************************");
-        print(e);
-        print("****************************");
         final snackBar = SnackBar(
           content: const Text("Try again"),
         );
@@ -77,9 +75,6 @@ class _LoginState extends State<Login> {
         setState(() {
           loading = false;
         });
-        print("****************************");
-        print(e);
-        print("****************************");
         final snackBar = SnackBar(
           content: const Text("Try again"),
         );
@@ -90,151 +85,151 @@ class _LoginState extends State<Login> {
     return loading
         ? Loading()
         : MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                primaryColor: Colors.red,
-                scaffoldBackgroundColor: const Color(0xFFEFEFEF)),
-            home: Scaffold(
-              resizeToAvoidBottomInset: false,
-              body: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.red,
+          scaffoldBackgroundColor: const Color(0xFFEFEFEF)),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(40),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("Redux",
-                                style: TextStyle(
-                                  fontSize: 50,
-                                )),
-                            Text("AWS",
-                                style: TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 50,
-                                ))
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        child: TextField(
-                          controller: emailController,
-                          cursorColor: Colors.black,
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(33),
-                                borderSide:
-                                    BorderSide(color: Colors.red, width: 2.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(33),
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 2.0),
-                              ),
-                              labelStyle: TextStyle(color: Colors.red),
-                              labelText: 'Login ID'),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        child: TextField(
-                          obscureText: true,
-                          controller: passwordController,
-                          cursorColor: Colors.black,
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(33),
-                                borderSide:
-                                    BorderSide(color: Colors.red, width: 2.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(33),
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 2.0),
-                              ),
-                              labelStyle: TextStyle(color: Colors.red),
-                              labelText: 'Password'),
-                        ),
-                      ),
-                      StoreConnector<AppState, AppState>(
-                        converter: (store) => store.state,
-                        builder: (context, state) {
-                          return Container(
-                            padding: EdgeInsets.all(10),
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: const RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(33))),
-                                primary: Colors.white,
-                                side: BorderSide(color: Colors.black, width: 2),
-                              ),
-                              onPressed: () {
-                                _login();
-                                emailController.clear();
-                                passwordController.clear();
-                                // StoreProvider.of<AppState>(context).dispatch(LogAction(
-                                //     emailController.text, passwordController.text));
-                                // Navigator.push(
-                                //     context, MaterialPageRoute(builder: (_) => Home()));
-                              },
-                              child: Text('SUBMIT',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 40,
-                                  )),
-                            ),
-                          );
-                        },
-                      ),
-                      Text("or"),
-                      StoreConnector<AppState, AppState>(
-                        converter: (store) => store.state,
-                        builder: (context, state) {
-                          return SingleChildScrollView(
-                            child: Container(
-                              alignment: Alignment.bottomCenter,
-                              padding: EdgeInsets.all(10),
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(33))),
-                                  primary: Colors.white,
-                                  side:
-                                      BorderSide(color: Colors.black, width: 2),
-                                ),
-                                onPressed: () {
-                                  _registerAccount();
-                                  // StoreProvider.of<AppState>(context).dispatch(LogAction(
-                                  //     emailController.text, passwordController.text));
-                                  // Navigator.push(
-                                  //     context, MaterialPageRoute(builder: (_) => Confirmation()));
-                                },
-                                child: Text('SIGNUP',
-                                    style: TextStyle(
-                                      color: Colors.red,
-                                      fontSize: 40,
-                                    )),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
+                      Text("Redux",
+                          style: TextStyle(
+                            fontSize: 50,
+                          )),
+                      Text("AWS",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 50,
+                          ))
                     ],
                   ),
                 ),
-              ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    controller: emailController,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(33),
+                          borderSide:
+                          BorderSide(color: Colors.red, width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(33),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                        labelStyle: TextStyle(color: Colors.red),
+                        labelText: 'Login ID'),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(33),
+                          borderSide:
+                          BorderSide(color: Colors.red, width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(33),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                        labelStyle: TextStyle(color: Colors.red),
+                        labelText: 'Password'),
+                  ),
+                ),
+                StoreConnector<AppState, AppState>(
+                  converter: (store) => store.state,
+                  builder: (context, state) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(33))),
+                          primary: Colors.white,
+                          side: BorderSide(color: Colors.black, width: 2),
+                        ),
+                        onPressed: () {
+                          _login();
+                          emailController.clear();
+                          passwordController.clear();
+                          // StoreProvider.of<AppState>(context).dispatch(LogAction(
+                          //     emailController.text, passwordController.text));
+                          // Navigator.push(
+                          //     context, MaterialPageRoute(builder: (_) => Home()));
+                        },
+                        child: Text('SUBMIT',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 40,
+                            )),
+                      ),
+                    );
+                  },
+                ),
+                Text("or"),
+                StoreConnector<AppState, AppState>(
+                  converter: (store) => store.state,
+                  builder: (context, state) {
+                    return SingleChildScrollView(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        padding: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(33))),
+                            primary: Colors.white,
+                            side:
+                            BorderSide(color: Colors.black, width: 2),
+                          ),
+                          onPressed: () {
+                            _registerAccount();
+                            // StoreProvider.of<AppState>(context).dispatch(LogAction(
+                            //     emailController.text, passwordController.text));
+                            // Navigator.push(
+                            //     context, MaterialPageRoute(builder: (_) => Confirmation()));
+                          },
+                          child: Text('SIGNUP',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 40,
+                              )),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-          );
+          ),
+        ),
+      ),
+    );
   }
 }
